c1da88954e3b5833eb7d7a0b0ccd89007a99e0b6
tdy81 <155242274+tdy81@users.noreply.github.com>
2023-12-30 18:25:44 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
 
-    $('button').click(function (e){
-        e.preventDefault();
-
-        var r = $('#input').val();
-
-        if(r === '435') {
-            console.log('ok')
-
-            $('#output').show()
-        }
-        else {
-            console.log('ko')
-        }
-    })
+    
 
 });
